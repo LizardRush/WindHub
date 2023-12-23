d7ab9423e8f332dac81f21a748df3040752de0ba
LizardRush <151949502+LizardRush@users.noreply.github.com>
2023-12-23 12:33:06 -0500

    Update host.py

--- repltemplates/flaskPython/host.py
+++ repltemplates/flaskPython/host.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Route for the homepage
+# Route for the homepage (rendering index.html)
 @app.route('/')
 def home():
-    return render_template_string('<h1>Welcome to the Website Host</h1>')
+    return render_template('index.html')
+
+# Route to serve static files (such as CSS, JS, images)
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return app.send_static_file(filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
